8a5145ae4f5bee7ccab00064c020bc0a3ae1cd4b
JK640327 <104485800+JK640327@users.noreply.github.com>
2022-08-14 19:02:12 +0800

    enemy fire
    to fire use spacebar

---
+++ iprog-vr/Assets/Scripts/EnemyOnMissile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOnMissile : MonoBehaviour
+{
+    private float speed = 10;
+    private Transform target;
+    private int enemyNum;
+    private int rng;
+
+    void Start()
+    {
+        enemyNum = Random.Range(1,3);
+        switch (enemyNum)
+        {
+            case (1):
+                target = GameObject.Find("Missile_turret").transform;
+                break;
+            case (2):
+                target = GameObject.Find("Minigun_turret Lvl 2").transform;
+                break;
+            case (3):
+                target = GameObject.Find("Missile_turret Lvl 1").transform;
+                break;
+            case (4):
+                target = GameObject.Find("Missile_turret").transform;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 tgtPos = target.position - transform.position;
+        Vector3 turRotation = Vector3.RotateTowards(transform.forward, tgtPos, speed * Time.deltaTime, 0);
+        transform.rotation = Quaternion.LookRotation(turRotation);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        rng = Random.Range(1, 100);
+        Debug.Log(rng);
+    }
+
+    public void LockTarget(Transform tgt)
+    {
+        target = tgt;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (rng <= 30)
+        {
+            Debug.Log("hit self");
+            Destroy(gameObject);
+        }
+        else
+        {
+            if(collision.gameObject.tag != "Enemy")
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ iprog-vr/Assets/Scripts/enemyShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyShoot : MonoBehaviour
+{
+    public Transform target;
+    public GameObject missile;
+    private GameObject newMissile;
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(FireEnemy());
+            }
+            Debug.DrawLine(missile.transform.position, target.position, Color.red, 200f);
+    }
+
+    IEnumerator FireEnemy()
+    {
+        newMissile = Instantiate(missile, transform.position, transform.rotation);
+        newMissile.SendMessage("LockTarget", target.transform);
+        yield return null;
+    }
+
+}
